dd979ab7ca9d372a5857b4e362ace632364a751e
root <root@DESKTOP-UM80NHI.localdomain>
2022-03-03 14:21:42 +0800

    refactor: rename

---
+++ shiro/cve_2020_13933/src/main/java/person/xu/vulEnv/ShiroBypassApplication.java
@@ -0,0 +1,13 @@
+package person.xu.vulEnv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroBypassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroBypassApplication.class, args);
+	}
+
+}
