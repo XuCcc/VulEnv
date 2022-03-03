cd1adf8a257e2575eb5237302d97f2dd18626153
root <root@DESKTOP-UM80NHI.localdomain>
2022-03-03 14:57:23 +0800

    feat: cve-2022-22947

---
+++ springboot/cve_2022_22947/src/main/java/person/xu/vulEnv/Cve202222947Application.java
@@ -0,0 +1,36 @@
+package person.xu.vulEnv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Cve202222947Application {
+    @GetMapping("/")
+    public String root() {
+        return "Gateway is up!";
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("path_route", r -> r.path("/get")
+                        .uri("http://httpbin.org"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .route("rewrite_route", r -> r.host("*.rewrite.org")
+                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+                        .uri("http://httpbin.org"))
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cve202222947Application.class, args);
+    }
+
+}
---
+++ springboot/cve_2022_22947/src/test/java/person/xu/vulEnv/Cve202222947ApplicationTests.java
@@ -0,0 +1,13 @@
+package person.xu.vulEnv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cve202222947ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
